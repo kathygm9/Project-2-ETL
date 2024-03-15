5f47a026b2324aaa72fb0ffc9da54946057ce093
Karthika Prasad <152231684+KarthikaPrasad1988@users.noreply.github.com>
2024-03-14 20:56:57 -0400

    Update crowdfunding_db_schema.sql-Edited create Queries

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,59 +1,45 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "Contacts" (
-    "contact_id" INT   NOT NULL,
-    "first_name" VARCHAR(50)   NOT NULL,
-    "last_name" VARCHAR(50)   NOT NULL,
-    "email" VARCHAR(100)   NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY (
-        "contact_id"
-     )
+CREATE TABLE Contacts
+(
+    contact_id integer NOT NULL,
+    first_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (contact_id)
 );
 
-CREATE TABLE "Category" (
-    "category_id" VARCHAR(50)   NOT NULL,
-    "category_name" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Category" PRIMARY KEY (
-        "category_id"
-     )
+
+CREATE TABLE Category
+(
+    category_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    category_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (category_id)
+)
+
+CREATE TABLE Subcategory
+(
+    subcategory_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    subcategory_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (subcategory_id)
 );
-
-CREATE TABLE "Subcategory" (
-    "subcategory_id" VARCHAR(50)   NOT NULL,
-    "subcategory_name" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
+CREATE TABLE Campaign
+(
+    cf_id integer NOT NULL,
+    contact_id integer NOT NULL,
+    company_name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    description character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    goal double precision NOT NULL,
+    pledged double precision NOT NULL,
+    outcome character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    backers_count integer NOT NULL,
+    country character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    currency character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    launched_date date NOT NULL,
+    end_date date NOT NULL,
+    category_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    subcategory_id character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (cf_id),
+    CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY (subcategory_id)
+        REFERENCES public."Subcategory" (subcategory_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
-
-CREATE TABLE "Campaign" (
-    "cf_id" INT   NOT NULL,
-    "contact_id" INT   NOT NULL,
-    "company_name" VARCHAR(255)   NOT NULL,
-    "description" VARCHAR(255)   NOT NULL,
-    "goal" INT   NOT NULL,
-    "pledged" INT   NOT NULL,
-    "outcome" VARCHAR(255)   NOT NULL,
-    "backers_count" INT   NOT NULL,
-    "country" VARCHAR(100)   NOT NULL,
-    "currency" VARCHAR(50)   NOT NULL,
-    "launched_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR(50)   NOT NULL,
-    "subcategory_id" VARCHAR(50)   NOT NULL,
-    CONSTRAINT "pk_Campaign" PRIMARY KEY (
-        "cf_id"
-     )
-);
-
-ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "Contacts" ("contact_id");
-
-ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
-REFERENCES "Category" ("category_id");
-
-ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "Subcategory" ("subcategory_id");
-
